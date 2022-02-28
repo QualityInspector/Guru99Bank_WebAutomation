33cf5f649f97ec55504827bb052ae887ff532984
QualityInspector <dhineshguna202@gmail.com>
2022-02-28 17:14:44 +0530

    Initial commit of whole project

---
+++ src/test/java/testSuites/login_TC01.java
@@ -0,0 +1,58 @@
+package testSuites;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import page_Objects.PO1_LoginPage;
+import page_Objects.PO2_HomePage;
+import settings.TestBase;
+
+public class login_TC01 extends TestBase {
+
+	//   mngr389366   qErubez
+	
+	WebDriver driver;
+	TestBase base = new TestBase();
+
+	@BeforeTest
+	public void openBrowser() {
+		driver = base.browserSettings();
+
+	}
+
+	@Test
+	public void loginPositive() throws InterruptedException {
+		PO1_LoginPage loginPage = new PO1_LoginPage(driver);
+
+		loginPage.userIdTextbox("mngr389366");
+		loginPage.passwordTextbox("qErubez");
+		loginPage.loginButton();
+
+		Thread.sleep(5000);
+
+		PO2_HomePage homePage = new PO2_HomePage(driver);
+
+		if (homePage.managerIdIdVisibility().isDisplayed()) {
+			System.out.println("Login Successful for +ve TC");
+			homePage.logoutButton();
+			acceptAlert(driver);
+			System.out.println("Alert accepted after clicking login button");
+		} else {
+			System.out.println("Login FAILED for +ve TC");
+			acceptAlert(driver);
+			System.out.println("Alert accepted after failed TC");
+		}
+
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+
+		driver.close();
+
+		System.out.println("Browser Closed");
+	}
+
+}
